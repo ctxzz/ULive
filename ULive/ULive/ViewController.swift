a097d6f06c2c25481766415d2b04ea8ad4e150f8
Atsushi OMATA <o_atsushi@takebay.net>
2017-08-22 18:18:43 +0900

    xcode project

---
+++ ULive/ULive/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ULive
+//
+//  Created by Atsushi OMATA on 2017/08/22.
+//  Copyright © 2017 Atsushi OMATA. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
